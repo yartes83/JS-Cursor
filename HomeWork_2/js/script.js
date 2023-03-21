3e18f4ec3a2fa2266f7473f2fed05e358e029893
Yaroslav Golub <yartes83@gmail.com>
2023-03-21 06:49:08 +0200

    HomeWork_2

--- HomeWork_2/js/script.js
+++ HomeWork_2/js/script.js
@@ -1,35 +1,35 @@
 let N = Number(prompt("Введіть значення N:"));
-console.log(N);
-document.writeln("Значення змінної N: " + N + "<br>" + "<br>");
+let M = Number(prompt("Введіть значення M"));
 
-if (!Number.isInteger(N)) {
-    alert("Прохання вводити цілі числа");
+while (!Number.isInteger(N) || Math.sign(N) === -1) {
+    alert('Прохання вводити цілі додатні числа');
+    N = Number(prompt("Введіть значення N:"));
 }
 
-
-let M = Number(prompt("Введіть значення M"));
-console.log(M);
-document.writeln("Значення змінної M: " + M + "<br>" + "<br>");
-if (!Number.isInteger(M)) {
-    alert("Прохання вводити цілі числа");
+while (!Number.isInteger(M) || N >= M || Math.sign(M) === -1) {
+    alert('Прохання ввести ціле додатнє число M, яке більше N');
+    M = Number(prompt("Введіть значення M:"));
 }
 
 
 let resultOddValue = window.confirm("Пропускаємо парні числа?");
 console.log(resultOddValue);
-document.writeln("Пропускаємо парні числа?: " + resultOddValue + "<br>" + "<br>");
+document.writeln("Пропускаємо парні числа : " + resultOddValue + "<br>" + "<br>");
+
 
 let sum = 0;
 
-if (resultOddValue) {
-    for (let i = N; i <= M; i++) {
-        if (i % 2 == 0) continue;
-        sum += i;
+    if (resultOddValue) {
+        for (let i = N; i <= M; i++) {
+            if (i % 2 === 0) continue;
+            sum += i;
+        }
+    } else {
+        for (let i = N; i <= M; i++) {
+            sum += i;
+        }
     }
-} else {
-    for (let i = N; i <= M; i++) {
-        sum += i;
-    }
-}
-console.log(sum)
+
+
+console.log(sum);
 document.writeln("Результат: " + sum + "<br>" + "<br>");
