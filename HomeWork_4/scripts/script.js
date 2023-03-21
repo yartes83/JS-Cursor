fbcc0dd62b2bd27a5af38ffdb7844ffd6ac1ef87
Yaroslav Golub <yartes83@gmail.com>
2023-03-21 10:21:10 +0200

    HomeWork_4

--- HomeWork_4/scripts/script.js
+++ HomeWork_4/scripts/script.js
@@ -1,9 +1,6 @@
 const students = ["Олександр", "Ігор", "Олена", "Іра", "Олексій", "Світлана"];
-
 const themes = ["Диференційне рівняння", "Теорія автоматів", "Алгоритми і структури даних"];
-
 const marks = [4, 5, 5, 3, 4, 5];
-
 
 //Завдання №1. Розділіть студентів на пари(хлопець + дівчина) для работи над проєктом:
 function getPairs(students) {
@@ -14,7 +11,7 @@ function getPairs(students) {
     return pairsRes;
 }
 const pairs = getPairs(students);
-console.log(pairs)
+console.log(pairs);
 
 //Завдання №2. Зіставте пари з попереднього завдання та теми проєктів, над якими студенти будуть працювати:
 function getThemesAndStudents (pairs, themes){
@@ -22,74 +19,24 @@ function getThemesAndStudents (pairs, themes){
 
     return result;
 }
-console.log(getThemesAndStudents(pairs, themes))
+console.log(getThemesAndStudents(pairs, themes));
 
 //Завдання №3. Зіставте оцінки(marks) зі студентом(students):
 function studentsMarks (students, marks) {
     let result = students.map(item => [item, marks[students.indexOf(item)]]);
     return result;
 }
-console.log(studentsMarks (students, marks))
+console.log(studentsMarks (students, marks));
 
 const themesAndStudents = getThemesAndStudents(pairs, themes);
-console.log(themesAndStudents)
+console.log(themesAndStudents);
 
 //Завдання №4. Поставте кожній парі випадкову оцінку(від 1 до 5):
 function getRandomMarks (themesAndStudents) {
     let randomMark = Math.floor(Math.random() * 5) + 1; // https://stackoverflow.com/questions/21483667/concept-of-math-floormath-random-5-1-what-is-the-true-range-and-why
     result = themesAndStudents.map(item => [item.join(', '),randomMark]);
 
-    return result
+    return result;
 }
 let groupResults = getRandomMarks (themesAndStudents);
-console.log(groupResults)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(groupResults);
