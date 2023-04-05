5bfc161a15618492d1aaf40524fe6ce753212fcd
Yaroslav Golub <yartes83@gmail.com>
2023-04-05 20:14:54 +0300

    HomeWork_8

--- HomeWork_8/scripts/script.js
+++ HomeWork_8/scripts/script.js
@@ -5,52 +5,44 @@ class Student {
         this.course = course;
         this.fullName = fullName;
         this.marks = marks;
+        this.isActive = true;
 
     }
 // 2. Створіть метод - повертає сукупну інформацію про курс, учбовий заклад та імені студента.
     getInfo() {
-        return (`Університет: ${this.university},  Курс: ${this.course}, ФІО: ${this.fullName}, Оцінки: ${this.marks}`);
+        return (`Університет: ${this.university},  Курс: ${this.course}, ФІО: ${this.fullName}, Оцінки: ${(this.marks.length===0 ? 'відсутні' : this.marks.join(', '))}`);
     }
 // 3. Створіть геттер оцінок this.marks, який повертає масив оцінок студента
     getMarks() {
-        return (`Оцінки студента: ${this.marks}`);
+        return this.isActive
+        ? `Оцінки студента: ${(this.marks.length===0 ? 'відсутні' : this.marks.join(', '))}`
+        : 'Студента відраховано';
     }
 
 // 4. Створіть сеттер оцінок this.marks = 5, який дозволяє поставити оцінку студенту.
     set mark(key) {
-        if (key === null) {
-             this.marks = null;
-        } else if (key === true){
-            if (this.marks === null) {
-                this.marks = [];
-            }
-             return  this.marks;
-        } else if (key !== null  && this.marks !== null) {
-            return this.marks.push(key);
-        } else {
-            return this.mark = null;
-        }
+       if (!this.isActive || !Number.isInteger(key) || key < 0) return;
+       this.marks.push(key);
     }
 
 // 5. Створіть метод отримання середнього балу this.getAverageMark()
 
     getAverageMark() {
-        let result = this.marks.reduce(function (prev, item) {
-            return prev + item;
-        }, 0);
-        return (result / this.marks.length).toFixed();
+        return this.isActive
+        ? (this.marks.reduce((a, b) => a + b, 0) / this.marks.length).toFixed()
+        : null;
     }
 // 6. Створіть метод this.dismiss, який "виключить" студента.
 // Після виклику цього методу – ставити студенту оцінки та отримувати їх більше не можна.
 // (Ніяких помилок, просто повертається завжди null замість масиву оцінок)
 
     dismiss() {
-        return this.mark = null;
+        this.isActive = false;
     }
 
 // 7. Створіть метод this.recover, який дозволить поновити студента
     recover() {
-        return this.mark = true
+        this.isActive = true;
 
 
     }
@@ -59,7 +51,7 @@ class Student {
 const student = new Student ('Університет Монстрів', 'Жарти і сміх дають енергію', 'Майкл Вазовськи', [5, 4]);
 console.log('клас Student: ', student);
 console.log('getInfo: ', student.getInfo());
-console.log(student.getMarks())
+console.log('getMarks: ', student.getMarks())
 student.mark = 5
 student.mark = 3
 student.mark = 4
@@ -68,14 +60,16 @@ console.log('getAverageMark: ', student.getAverageMark())
 console.log('===========================')
 student.dismiss()
 
-console.log(student.getMarks())
+
+
+console.log('отримаємо оцінки: ', student.getMarks())
 console.log('===========================')
 student.recover()
-console.log(student.mark)
 
-student.mark = 5
-student.mark = 5
-student.mark = 5
+
+student.mark = 3
+student.mark = 2
+student.mark = 1
 console.log(student)
 
 
